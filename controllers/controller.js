e8b2914df3dd0a3bf03f6d473c20f46a9a3eba55
dikinovtrianda <dikinovtrianda@gmail.com>
2025-04-08 15:49:21 +0700

    feat : login

---
+++ controllers/controller.js
@@ -0,0 +1,12 @@
+const { signToken } = require("../helpers/jwt");
+
+module.exports = class Controller {
+    static login(req, res) {
+        const { username } = req.body;
+        if (!username) {
+            return res.status(400).json({ error: "Username is required" });
+        }
+        const access_token = signToken({ username })
+        res.status(200).json({ access_token });
+    }
+}

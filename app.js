93ac6bdbbe73e780db79510be9836a2ebe5a6eeb
dikinovtrianda <dikinovtrianda@gmail.com>
2025-04-09 18:39:02 +0700

    feat : adding game queue socket

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+const Controller = require('./controllers/controller')
 const express = require('express')
 const app = express()
-const port = 3000
 const cors = require('cors')
-const Controller = require('./controllers/controller')
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+const room1 = []
 
 app.use(cors())
 app.use(express.json())
@@ -14,9 +23,40 @@ app.use(express.urlencoded({ extended: true }))
 app.post('/login', Controller.login)
 app.get('/username', Controller.getUsername)
 app.get('/questions', Controller.getQuestion) 
-
 app.get('/hint', Controller.getHint)
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  
+  // const token = socket.handshake.auth.token;
+  // console.log("Token received:", token);
+
+  socket.on("game queue", (params) => {
+    console.log(params, "<<< message dari client");
+    socket.join("room1")
+
+    const existingUser = room1.find((user) => user.socketId === socket.id);
+    if (!existingUser) {
+      room1.push({ socketId: socket.id, username: params.username });
+      console.log(room1, "<<< room1");
+      
+    }
+    io.to("room1").emit("game queue", room1)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+
+    const index = room1.findIndex((user) => user.socketId === socket.id);
+    if (index !== -1) {
+      room1.splice(index, 1);
+    }
+
+    io.to("room1").emit("game queue", room1);
+  });
+});
+
+const port = 3000
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

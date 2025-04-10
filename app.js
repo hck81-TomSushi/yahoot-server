5b3ebf12893be4c34621c1d72449ecdcedb81958
Hilmi Ahmad <hilmiadd@gmail.com>
2025-04-10 11:28:25 +0700

    config: deploy configuration

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const port = 3000
+const port = process.env.PORT || 3000
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
